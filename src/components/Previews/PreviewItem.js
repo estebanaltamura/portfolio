9e017c70fb03d63bb5e1efd2f3b21decbb0ca306
estebanaltamura <esteban.altamura@gmail.com>
2023-07-24 17:36:46 -0300

    actualñizacion

--- src/components/Previews/PreviewItem.js
+++ src/components/Previews/PreviewItem.js
@@ -1,13 +1,43 @@
+import Carousel from 'react-bootstrap/Carousel';
+import { v4 as randomId } from 'uuid'
 import "./PreviewItem.css"
 
 export const PreviewItem = ({ type, img })=>{    
 
   return(
-    <div className={type == "desktop" ? "previewMediaDesktop" : "previewMediaMobile"} >      
-      <img className={type == "desktop" ? "desktopPreviewImage previewImage" : "mobilePreviewImage previewImage"} src={img} />       
+    <div className={type == "desktop" ? "previewMediaDesktop" : "previewMediaMobile"} > 
+      <div id={type} class="carousel slide">
+        <div class="carousel-inner">
+          
+          <div class="carousel-item active">
+            <img className={type == "desktop" ? "desktopPreviewImage previewImage" : "mobilePreviewImage previewImage"} src={img} /> 
+          </div>
+          
+          <div class="carousel-item">
+            <img className={type == "desktop" ? "desktopPreviewImage previewImage" : "mobilePreviewImage previewImage"} src={img} /> 
+          </div>
+    
+          <div class="carousel-item">
+            <img className={type == "desktop" ? "desktopPreviewImage previewImage" : "mobilePreviewImage previewImage"} src={img} /> 
+          </div>
+
+        </div>
+      
+        <button class="carousel-control-prev controlContainer-prev" type="button" data-bs-target={`#${type}`} data-bs-slide="prev">
+          <span class="carousel-control-prev-icon controlIcon-prev" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+
+        <button class="carousel-control-next controlContainer-next" type="button" data-bs-target={`#${type}`} data-bs-slide="next">
+          <span class="carousel-control-next-icon controlIcon-next" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span> 
+        </button>
+      </div>
     </div>
   )
 }
+
+
 
 
 
