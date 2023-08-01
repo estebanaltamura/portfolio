539213a74897da3607822fdf85c24562aab57dcd
estebanaltamura <esteban.altamura@gmail.com>
2023-08-01 13:18:16 -0300

    actualizacion

--- src/JSONs/JSONprojects.js
+++ src/JSONs/JSONprojects.js
@@ -132,18 +132,9 @@ export const projectsData = [
     technologies: [
       { name: "HTML", iconSrc: "https://i.postimg.cc/kGPKdGc8/HtmlIcon.png" },
       { name: "CSS", iconSrc: "https://i.postimg.cc/3Rvm6mgY/cssIcon.png" },
-      {
-        name: "Javascript",
-        iconSrc: "https://i.postimg.cc/7LHby70S/javascript-Icon.png",
-      },
-      {
-        name: "React",
-        iconSrc: "https://i.postimg.cc/MZ11m06Y/react-Icon.png",
-      },
-      {
-        name: "Firebase",
-        iconSrc: "https://i.postimg.cc/Kj7LcbqW/firebase-Icon.png",
-      },
+      { name: "Javascript", iconSrc: "https://i.postimg.cc/7LHby70S/javascript-Icon.png" },
+      { name: "React", iconSrc: "https://i.postimg.cc/MZ11m06Y/react-Icon.png" },
+      { name: "Firebase", iconSrc: "https://i.postimg.cc/Kj7LcbqW/firebase-Icon.png" }
     ],
     desktopPreviews:
     [
@@ -162,123 +153,10 @@ export const projectsData = [
       "https://i.postimg.cc/FRBYCqv4/carro-mobile-1.jpg"
     ],
       
-    // desktopPreview:
-    //   "https://firebasestorage.googleapis.com/v0/b/portafolio-b98ea.appspot.com/o/2023-04-25 15-01-23.mp4?alt=media&token=https://firebasestorage.googleapis.com/v0/b/portafolio-b98ea.appspot.com/o/2023-04-25%2015-01-23.mp4?alt=media&token=d4a6d705-7592-4afc-8707-d631be48616a",
-    // mobilePreview:
-    //   "https://firebasestorage.googleapis.com/v0/b/portafolio-b98ea.appspot.com/o/az_recorder_20230425_160007.mp4?alt=media&token=https://firebasestorage.googleapis.com/v0/b/portafolio-b98ea.appspot.com/o/az_recorder_20230425_160007.mp4?alt=media&token=bf8116a1-72a1-4a65-9c8d-2c5310b69fc1",
-    gitHubLink: "https://github.com/estebanaltamura/entregaFinal-Altamura",
-    webSiteLink: "https://entrega-final-altamura.vercel.app/",
-  },
-
-  {
-    name: "Photo assistant",
-    projectSummary: [
-      "PANDORA backpacks is a project which is a whole e-commerce developed in React using Firebase to saved the information of orders and products",
-      "PANDORA backpacks is a project which is a whole e-commerce developed in React using Firebase to saved the information of orders and products",
-      "PANDORA backpacks is a project which is a whole e-commerce developed in React using Firebase to saved the information of orders and products",
-      "PANDORA backpacks is a project which is a whole e-commerce developed in React using Firebase to saved the information of orders and products",
-    ],
-    projectDetails: [
-      "PANDORA backpacks is a project which is a whole e-commerce developed in React using Firebase to saved the information of orders and products",
-      "PANDORA backpacks is a project which is a whole e-commerce developed in React using Firebase to saved the information of orders and products",
-      "PANDORA backpacks is a project which is a whole e-commerce developed in React using Firebase to saved the information of orders and products",
-      "PANDORA backpacks is a project which is a whole e-commerce developed in React using Firebase to saved the information of orders and products",
-    ],
-    technologies: [
-      { name: "HTML", iconSrc: "https://i.postimg.cc/kGPKdGc8/HtmlIcon.png" },
-      { name: "CSS", iconSrc: "https://i.postimg.cc/3Rvm6mgY/cssIcon.png" },
-      {
-        name: "Javascript",
-        iconSrc: "https://i.postimg.cc/7LHby70S/javascript-Icon.png",
-      },
-      {
-        name: "React",
-        iconSrc: "https://i.postimg.cc/MZ11m06Y/react-Icon.png",
-      },
-      {
-        name: "Firebase",
-        iconSrc: "https://i.postimg.cc/Kj7LcbqW/firebase-Icon.png",
-      },
-    ],
-    desktopPreviews:
-    [
-      "https://i.postimg.cc/g0cyffCX/home-desktop-1.jpg",
-      "https://i.postimg.cc/VkWBZYVP/home-desktop-2.jpg",
-      "https://i.postimg.cc/28vxvdd6/colecciones-desktop-1.jpg",
-      "https://i.postimg.cc/2j1GjNVn/carro-desktop-1.jpg"
-    ],
-      
-    mobilePreviews:
-    [
-      "https://i.postimg.cc/5yQ6h39F/home-mobile-1.jpg",
-      "https://i.postimg.cc/G90Hdy9g/home-mobile-2.jpg",
-      "https://i.postimg.cc/C1zGKbLw/home-mobile-3.jpg",
-      "https://i.postimg.cc/mDDcw40j/colecciones-mobile-1.jpg",
-      "https://i.postimg.cc/FRBYCqv4/carro-mobile-1.jpg"
-    ],
-      
-    // desktopPreview:
-    //   "https://firebasestorage.googleapis.com/v0/b/portafolio-b98ea.appspot.com/o/2023-04-25 15-01-23.mp4?alt=media&token=https://firebasestorage.googleapis.com/v0/b/portafolio-b98ea.appspot.com/o/2023-04-25%2015-01-23.mp4?alt=media&token=d4a6d705-7592-4afc-8707-d631be48616a",
-    // mobilePreview:
-    //   "https://firebasestorage.googleapis.com/v0/b/portafolio-b98ea.appspot.com/o/az_recorder_20230425_160007.mp4?alt=media&token=https://firebasestorage.googleapis.com/v0/b/portafolio-b98ea.appspot.com/o/az_recorder_20230425_160007.mp4?alt=media&token=bf8116a1-72a1-4a65-9c8d-2c5310b69fc1",
-    gitHubLink: "https://github.com/estebanaltamura/entregaFinal-Altamura",
-    webSiteLink: "https://entrega-final-altamura.vercel.app/",
-  },
-
-  {
-    name: "Mario 3",
-    projectSummary: [
-      "Pandora Mochilas es un ecommerce simulado de mochilas desarrollado en React que integra todos los procesos menos la integración con un medio de pago.",
-    ],
-    projectDetails: [
-      "Persiste los datos del carro de compras en el local storage para poder recuperar la información ante una recarga o cierre de la página. El evento que dispara la carga de datos al storage es el unload y en el load se descargan. Mientras la página está en uso con normalidad no se persisten los datos del carro en el storage. Si en un contexto global",
-      "Se usaron métodos para que las imágenes no carguen una a una sino que el usuario vea las imágenes aparecer en un solo momento",
-      "Mientras se espera que todo esté cargado para mostrarle al usuario se utilizaron estados loading para colocar un spinner en el proceso",
-      "Hay un contexto global para los artículos agregados al carrito",
-      "Las imágenes están hosteadas en un storage de imágenes",
-      "Tanto los productos con todas sus características y rutas a recursos como las órdenes, se persisten en Firebase",
-      "Se crearon dos escenarios para la finalización de una compra. El pago con tarjeta en el cual como última pantalla muestra el objeto que se le pasaría a MercadoPago en el caso que este proyecto hubiese montado un servidor y se hubiese integrado MercadoPago y la otra opción es una opción custom en el cual hay que dejar los datos para que el vendedor luego lo contacte, en el cual el ecommerce persiste en Firebase todos los productos de la transacción más los datos que completó el usuario en el formulario en una orden.",
-    ],
-    technologies: [
-      { name: "HTML", iconSrc: "https://i.postimg.cc/kGPKdGc8/HtmlIcon.png" },
-      { name: "CSS", iconSrc: "https://i.postimg.cc/3Rvm6mgY/cssIcon.png" },
-      {
-        name: "Javascript",
-        iconSrc: "https://i.postimg.cc/7LHby70S/javascript-Icon.png",
-      },
-      {
-        name: "React",
-        iconSrc: "https://i.postimg.cc/MZ11m06Y/react-Icon.png",
-      },
-      {
-        name: "Firebase",
-        iconSrc: "https://i.postimg.cc/Kj7LcbqW/firebase-Icon.png",
-      },
-    ],
-    desktopPreviews:
-    [
-      "https://i.postimg.cc/g0cyffCX/home-desktop-1.jpg",
-      "https://i.postimg.cc/VkWBZYVP/home-desktop-2.jpg",
-      "https://i.postimg.cc/28vxvdd6/colecciones-desktop-1.jpg",
-      "https://i.postimg.cc/2j1GjNVn/carro-desktop-1.jpg"
-    ],
-      
-    mobilePreviews:
-    [
-      "https://i.postimg.cc/5yQ6h39F/home-mobile-1.jpg",
-      "https://i.postimg.cc/G90Hdy9g/home-mobile-2.jpg",
-      "https://i.postimg.cc/C1zGKbLw/home-mobile-3.jpg",
-      "https://i.postimg.cc/mDDcw40j/colecciones-mobile-1.jpg",
-      "https://i.postimg.cc/FRBYCqv4/carro-mobile-1.jpg"
-    ],
-      
-    // desktopPreview:
-    //   "https://firebasestorage.googleapis.com/v0/b/portafolio-b98ea.appspot.com/o/2023-04-25 15-01-23.mp4?alt=media&token=https://firebasestorage.googleapis.com/v0/b/portafolio-b98ea.appspot.com/o/2023-04-25%2015-01-23.mp4?alt=media&token=d4a6d705-7592-4afc-8707-d631be48616a",
-    // mobilePreview:
-    //   "https://firebasestorage.googleapis.com/v0/b/portafolio-b98ea.appspot.com/o/az_recorder_20230425_160007.mp4?alt=media&token=https://firebasestorage.googleapis.com/v0/b/portafolio-b98ea.appspot.com/o/az_recorder_20230425_160007.mp4?alt=media&token=bf8116a1-72a1-4a65-9c8d-2c5310b69fc1",
-    gitHubLink: "https://github.com/estebanaltamura/entregaFinal-Altamura",
-    webSiteLink: "https://entrega-final-altamura.vercel.app/",
-  },
+    
+    gitHubLink: "https://github.com/estebanaltamura/actasitalianasexpress",
+    webSiteLink: "https://https://www.actasitalianasexpress.com/",
+  }
 ];
 
 /*
