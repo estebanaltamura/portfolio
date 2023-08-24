79eff1e880fd524429476edded2c565163bf64b3
estebanaltamura <esteban.altamura@gmail.com>
2023-08-24 14:26:24 -0300

    actualizacion

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -8,14 +8,18 @@ export const Welcome = () => {
         <p className="welcomeParagraphs">Soy Andrés Altamura.
         </p>
         <p className="welcomeParagraphs">
-           De chico me conecté con el desarrollo y el mundo de la computación pero no profundicé. En los últimos años empecé a codear de nuevo y me di cuenta que es algo que me apasiona. A raíz de esto apliqué todos mis conocimientos y co-fundé una empresa de venta de indumentaria en los Estados Unidos que implicó un desafío enorme. 
-        </p>
-        <p className="welcomeParagraphs">Haber emprendido muchos años en diversos proyectos hicieron que me transforme en un profesional versátil, capaz de trabajar con equipos multidisciplinarios y desarrollando la habilidad de gestionar eficazmente la ambigüedad de los proyectos complejos. 
+          De chico me conecté con el desarrollo y el mundo de la computación pero no profundicé. En los últimos años empecé a codear de nuevo y me di cuenta que es algo que me apasiona. A raíz de esto apliqué todos mis conocimientos y co-fundé una empresa de venta de indumentaria en los Estados Unidos que implicó un desafío enorme.
         </p>
         <p className="welcomeParagraphs">
-        Con mi experiencia en tecnología y emprendedurismo sé que puedo aportar mis habilidades y conocimientos a cualquier empresa.
+          Haber emprendido muchos años en diversos proyectos hizo que me transforme en un profesional versátil, capaz de trabajar con equipos multidisciplinarios y con la habilidad de gestionar eficazmente la ambigüedad de los proyectos complejos.
+          </p>
+        <p className="welcomeParagraphs">
+          Con mi experiencia en tecnología y emprededurismo, sé que puedo aportar mis habilidades y conocimientos a cualquier empresa.
         </p>	         
       </div>
     </div>
   );
 };
+
+
+
