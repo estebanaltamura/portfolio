b66f2dde0d70bd198c0be8f526d8c0c73e61da9b
estebanaltamura <esteban.altamura@gmail.com>
2023-08-29 11:07:18 -0300

    actualizacion

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -8,18 +8,16 @@ export const Welcome = () => {
         <p className="welcomeParagraphs">Soy Andrés Altamura.
         </p>
         <p className="welcomeParagraphs">
-          De chico me conecté con el desarrollo y el mundo de la computación pero no profundicé. En los últimos años empecé a codear de nuevo y me di cuenta que es algo que me apasiona. A raíz de esto apliqué todos mis conocimientos y co-fundé una empresa de venta de indumentaria en los Estados Unidos que implicó un desafío enorme.
+          De chico me conecté con el desarrollo y el mundo de la informática, pero al salir de la secundaria me incliné por el mundo emprendedor.
         </p>
         <p className="welcomeParagraphs">
           Haber emprendido muchos años en diversos proyectos hizo que me transforme en un profesional versátil, capaz de trabajar con equipos multidisciplinarios y con la habilidad de gestionar eficazmente la ambigüedad de los proyectos complejos.
           </p>
         <p className="welcomeParagraphs">
-          Con mi experiencia en tecnología y emprededurismo, sé que puedo aportar mis habilidades y conocimientos a cualquier empresa.
+          Con mi experiencia en tecnología y emprededurismo, sé que puedo aportar mis habilidades y conocimientos en pos de generar valor.
         </p>	         
       </div>
     </div>
   );
 };
-
-
 
