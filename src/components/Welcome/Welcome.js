14f7f1b268d77ce87d1e973edd3525c22de4494f
Andres Altamura <esteban.altamura@gmail.com>
2024-01-11 14:01:48 -0300

    copy home actualizado

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -9,23 +9,23 @@ export const Welcome = () => {
           Mi nombre es Andrés, soy desarrollador Frontend React-Typescript.
         </p>
         <p className='welcomeParagraphs'>
-          Soy un perfil startup, me gustan los emprendimientos, creo en los
-          procesos agiles, dinamicos y de mucha iteracion con los usuarios y el
+          Tengo un perfil startup, me gustan los emprendimientos, creo en los
+          procesos ágiles, dinámicos y de mucha iteración con los usuarios y el
           mercado.
         </p>
         <p className='welcomeParagraphs'>
-          En los últimos años trabaje en una incubadora de negocios como
-          referente tecnologico, co-fundé una empresa de venta de indumentaria
-          en los Estados Unidos, la cual fue un desafio enorme y en la cual
-          aprendí y profundice cantidad de skills. También co-fundé una empresa
-          de venta de indumentaria en Argentina de buen posicionamiento y
-          solidas ventas.
+          En los últimos años trabajé en una incubadora de negocios como
+          referente tecnológico, co-fundé una empresa de venta de indumentaria
+          en los Estados Unidos, la que fue un desafío enorme y en la cual
+          aprendí y profundicé variedad y cantidad de skills. También co-fundé
+          una empresa de venta de indumentaria en Argentina de buen
+          posicionamiento y solidas ventas.
         </p>
         <p className='welcomeParagraphs'>
           Haber emprendido muchos años en diversos proyectos y trabajado en
           ecosistemas de negocios hizo que me transforme en un profesional
           versátil, capaz de trabajar con equipos multidisciplinarios y con la
-          habilidad de gestionar eficazmente la ambigüedad y la dinamica de los
+          habilidad de gestionar eficazmente la ambigüedad y la dinámica de los
           proyectos complejos.
         </p>
       </div>
