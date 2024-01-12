2f1a4b176255f7b2b3f59ae84ba6178fc4676229
Andres Altamura <esteban.altamura@gmail.com>
2024-01-12 09:01:40 -0300

    actualizacion

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -9,17 +9,17 @@ export const Welcome = () => {
           Mi nombre es Andrés, soy desarrollador Frontend React-Typescript.
         </p>
         <p className='welcomeParagraphs'>
-          Tengo un perfil startup, me gustan los emprendimientos, creo en los
-          procesos ágiles, dinámicos y de mucha iteración con los usuarios y el
-          mercado.
+          Soy desarrollador Frontend, tengo un perfil startup, me gustan los
+          emprendimientos, creo en los procesos ágiles, dinámicos y de mucha
+          iteración con los usuarios y el mercado.
         </p>
         <p className='welcomeParagraphs'>
           En los últimos años trabajé en una incubadora de negocios como
           referente tecnológico, co-fundé una empresa de venta de indumentaria
-          en los Estados Unidos, la que fue un desafío enorme y en la cual
-          aprendí y profundicé variedad y cantidad de skills. También co-fundé
-          una empresa de venta de indumentaria en Argentina de buen
-          posicionamiento y solidas ventas.
+          en Argentina con excelente posicionamiento y solidas ventas. Tambien
+          co-fundé una empresa de venta de indumentaria en Estados Unidos. En
+          esta última experiencia casi quiebro, pero entendí que esto es lo mio,
+          me encanta emprender y el proceso startup en su conjunto.
         </p>
         <p className='welcomeParagraphs'>
           Haber emprendido muchos años en diversos proyectos y trabajado en
